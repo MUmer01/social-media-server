f31d0fdfbdfddb0abbbaa4aae2e0ee78d15d8c9a
Muhammad Umer <umerts3@gmail.com>
2021-09-12 03:38:05 +0500

    update cors

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: "*",
-//     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH", "OPTIONS"],
-//   })
-// );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH", "OPTIONS"],
+  })
+);
 
 const userRoute = require("./routes/User");
 app.use("/user", userRoute);
