41c80bfcc3d8ce308944bb0ebac8d39953898a75
Muhammad Umer <umerts3@gmail.com>
2021-09-12 03:28:07 +0500

    add option

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(
-    cors({
-        origin: "*",
-        methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-    })
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH", "OPTIONS"],
+  })
 );
 app.use(express.json());
 
@@ -15,9 +15,9 @@ const postsRoute = require("./routes/Posts");
 app.use("/posts", postsRoute);
 
 app.get("/", (req, res) => {
-    res.send("Nothing is here!");
+  res.send("Nothing is here!");
 });
 
 app.listen(process.env.PORT || 3001, (req, res) => {
-    console.log(`Server running on port ${process.env.PORT || 3001}`);
+  console.log(`Server running on port ${process.env.PORT || 3001}`);
 });
