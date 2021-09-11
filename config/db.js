2281b7b417e113ea1dd320bb2a7076dcd7fec003
Muhammad Umer <umerts3@gmail.com>
2021-09-12 03:46:58 +0500

    update connection

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASS || "",
-    database: process.env.DB || "socialmedia",
+const db = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "",
+  database: process.env.DB || "socialmedia",
 });
 
 module.exports = db;
